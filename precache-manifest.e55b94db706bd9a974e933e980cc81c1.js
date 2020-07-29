10921bd040c5c5226d848286100b89ea5e07136d
akademia108 <kursy@akademia108.pl>
2020-07-29 12:36:35 +0200

    Updates

---
+++ precache-manifest.e55b94db706bd9a974e933e980cc81c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f555434fc4fe09e4e971c70596fce1",
+    "url": "/example-gh-pages/index.html"
+  },
+  {
+    "revision": "9e46635640e927d3306c",
+    "url": "/example-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "159ab9f43a6e6ef99d41",
+    "url": "/example-gh-pages/static/js/2.c1dc65fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example-gh-pages/static/js/2.c1dc65fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e46635640e927d3306c",
+    "url": "/example-gh-pages/static/js/main.fcbeb453.chunk.js"
+  },
+  {
+    "revision": "826c80b205b8f6700207",
+    "url": "/example-gh-pages/static/js/runtime-main.ff4b4e43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
